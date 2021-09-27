359061ddead74a993e5150e12557e0683899c0cd
Patryk Langer <patryklanger@icloud.com>
2021-09-27 16:32:50 +0200

    menu finished

--- src/app/elements/menu/menu.component.ts
+++ src/app/elements/menu/menu.component.ts
@@ -1,10 +1,34 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100vw)' }),
+        animate('0.3s ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)' }),
+        animate('0.3s ease-in', style({ transform: 'translateX(-100vw)' })),
+      ]),
+    ]),
+    trigger('holdUntilAnimate', [
+      transition(':enter', [
+        style({ display: 'none' }),
+        animate('0.3s', style({ display: 'block' })),
+      ]),
+    ]),
+  ],
 })
 export class MenuComponent implements OnInit {
   opened = false;
